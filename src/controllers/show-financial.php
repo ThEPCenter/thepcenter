23b46912c825bbafe6ca9d2f1d89aecb44583ff2
Suttipong Srikok <fisca2012@gmail.com>
2014-01-16 11:00:25 +0700

    Update links in "show-financial".

--- src/controllers/show-financial.php
+++ src/controllers/show-financial.php
@@ -2,20 +2,20 @@
 <h2 class="text-center">แบบฟอร์ม : ฝ่ายการเงินและบัญชี</h2>    
 <div class="row">
     <div class="col-md-8 col-md-offset-3">
-        <p><a href="../files/fiacc/ThEP-FI-01.doc">1. ThEP-FI-01 ขอยืมเงิน</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-02.doc">2. ThEP-FI-02 แบบรายงานการเดินทาง</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-02-1.doc">3. ThEP-FI-02-1 แบบรายงานการเดินทาง(ใบแนบกรณีเดินทางเป็นหมู่คณะ)</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-03.doc">4. ThEP-FI-03 แบบเบิกจ่ายเงิน</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-04.doc">5. ThEP-FI-04 ใบสำคัญรับเงิน</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-04-1.doc">6. ThEP-FI-04-1 ใบสำคัญรับเงิน - ทุนนักศึกษา</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-05.doc">7. ThEP-FI-05 ใบรับรองแทนใบเสร็จรับเงิน</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-05-1.doc">8. ThEP-FI-05-1 ใบรับรองแทนใบเสร็จรับเงิน(โครงการวิจัย)</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-06.doc">9. ThEP-FI-06 ปรับงบประมาณศูนย์วิจัย</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-06-1.doc">10. ThEP-FI-06-1 ปรับงบประมาณห้องปฏิบัติการวิจัย</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-07.doc">11. ThEP-FI-07 ปรับงบประมาณโครงการวิจัย</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-08.doc">12. ThEP-FI-08 แบบรายงานการใช้เงินทดรองจ่ายศูนย์วิจัย</a></p>
-        <p><a href="../files/fiacc/ThEP-FI-01-A.doc">13. ตัวอย่างหนังสือขอเดินทางไปราชการ</a></p>
-        <p><a href="../files/fiacc/Regulation-Finance&Accouting-54.pdf" target="_blank">14. ระเบียบศูนย์ความเป็นเลิศด้านฟิสิกส์ ว่าด้วยการเงิน การบัญชี และการงบประมาณ พ.ศ. 2554</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-01.doc">1. ThEP-FI-01 ขอยืมเงิน</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-02.doc">2. ThEP-FI-02 แบบรายงานการเดินทาง</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-02-1.doc">3. ThEP-FI-02-1 แบบรายงานการเดินทาง(ใบแนบกรณีเดินทางเป็นหมู่คณะ)</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-03.doc">4. ThEP-FI-03 แบบเบิกจ่ายเงิน</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-04.doc">5. ThEP-FI-04 ใบสำคัญรับเงิน</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-04-1.doc">6. ThEP-FI-04-1 ใบสำคัญรับเงิน - ทุนนักศึกษา</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-05.doc">7. ThEP-FI-05 ใบรับรองแทนใบเสร็จรับเงิน</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-05-1.doc">8. ThEP-FI-05-1 ใบรับรองแทนใบเสร็จรับเงิน(โครงการวิจัย)</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-06.doc">9. ThEP-FI-06 ปรับงบประมาณศูนย์วิจัย</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-06-1.doc">10. ThEP-FI-06-1 ปรับงบประมาณห้องปฏิบัติการวิจัย</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-07.doc">11. ThEP-FI-07 ปรับงบประมาณโครงการวิจัย</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-08.doc">12. ThEP-FI-08 แบบรายงานการใช้เงินทดรองจ่ายศูนย์วิจัย</a></p>
+        <p><a href="../files/form_fiacc/ThEP-FI-01-A.doc">13. ตัวอย่างหนังสือขอเดินทางไปราชการ</a></p>
+        <p><a href="../files/form_fiacc/Regulation-Finance&Accouting-54.pdf" target="_blank">14. ระเบียบศูนย์ความเป็นเลิศด้านฟิสิกส์ ว่าด้วยการเงิน การบัญชี และการงบประมาณ พ.ศ. 2554</a></p>
     </div>
 </div>
 <p>&nbsp;</p>
