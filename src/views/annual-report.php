6f68d888e4638b94af740cacf474caed0b912910
Suttipong Srikok <fisca2012@gmail.com>
2014-02-04 17:37:26 +0700

    Add annual report 2011 data.

--- src/views/annual-report.php
+++ src/views/annual-report.php
@@ -2,7 +2,10 @@
 require_once '../system/system.php';
 doc_head('รายงานประจำปี - ศูนย์ความเป็นเลิศด้านฟิสิกส์');
 ?>
+<script>
 
+</script>
+    
 </head>
 
 <body>
@@ -11,8 +14,14 @@ doc_head('รายงานประจำปี - ศูนย์ความ�
         <?php get_includes('header'); ?>
 
         <div class="row">
-            <h2 class="text-center">รายงานประจำปี</h2>
-            <img class="img-responsive" style="margin:0 auto;" src="../images/under-construction.jpg">
+            <div class="col-md-12">
+                <h2 class="text-center">รายงานประจำปี</h2>
+                <h3 id="54">รายงานประจำปี 2554</h3>
+                <ol>
+                    <li><a href="../../upload02/ThEP_Annual_Report_2011/01_cover-history.pdf" target="_blank">หน้าปก ประวัติความเป็นมา</a></li>
+                    <li><a href="../../upload02/ThEP_Annual_Report_2011/2_TFP.pdf" target="_blank">ศูนย์วิจัยทางฟิสิกส์ของฟิล์มบาง (TFP)</a></li>
+                </ol>
+            </div>            
         </div>
 
         <?php get_includes('footer'); ?>
