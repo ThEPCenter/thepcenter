b2559892b85bf569ac86d5afddffd9902eb56780
Suttipong Srikok <fisca2012@gmail.com>
2014-04-29 16:47:30 +0700

    Edit home for NSP's pic.

--- src/views/home.php
+++ src/views/home.php
@@ -284,7 +284,7 @@ if ($_SESSION['fancy'] == "first") {
 
             <div class="col-xs-6 col-sm-2 col-md-2">
                 <a title="ศูนย์วิจัยทางนาโนสเกลฟิสิกส์" href="research-center.php?show_center=NSP">
-                    <img style="margin: 0 auto; border: 1px solid #666666;" class="img-responsive" src="../images/center/NSP_red_oled_592.jpg">
+                    <img style="margin: 0 auto; border: 1px solid #666666;" class="img-responsive" src="../images/center/NSP_red_oled_300.jpg">
                     <p style="font-size: 90%;">ศูนย์วิจัยทางนาโนสเกลฟิสิกส์</p>
                 </a>
             </div>
