679e02b61a3edfc0264407741cf9bcafc7f5bf2d
Suttipong Srikok <fisca2012@gmail.com>
2014-10-03 16:00:38 +0700

    Create researcher search and display profile.

--- src/views/researcher.php
+++ src/views/researcher.php
@@ -1,29 +1,22 @@
 <?php
 require_once '../system/system.php';
-doc_head('ฐานข้อมูลนักวิจัย - ศูนย์ความเป็นเลิศด้านฟิสิกส์');
+doc_head('ฐานข้อมูลนักวิจัย ศูนย์ความเป็นเลิศด้านฟิสิกส์');
 
 if (isset($_POST['keyword']) && !empty($_POST['keyword'])) {
     $field_search = $_POST['field_search'];
     $kw = htmlspecialchars($_POST['keyword'], ENT_QUOTES);
     $kw_ori = $kw;
     $kw = "%$kw%";
-
     if ($field_search == 'firstname') {
-        $sql_all = "SELECT * FROM res_profile
-            WHERE (firstname_th LIKE '$kw') OR (firstname_en LIKE '$kw');";
+        $sql_all = "SELECT * FROM res_profile WHERE (firstname_th LIKE '$kw') OR (firstname_en LIKE '$kw') ORDER BY firstname_en;";
     } elseif ($field_search == 'lastname') {
-        $sql_all = "SELECT * FROM res_profile
-            WHERE (lastname_th LIKE '$kw') OR (lastname_en LIKE '$kw');";
+        $sql_all = "SELECT * FROM res_profile WHERE (lastname_th LIKE '$kw') OR (lastname_en LIKE '$kw') ORDER BY lastname_en;";
     } elseif ($field_search == 'institute') {
-        $sql_all = "SELECT * FROM res_employment
-            WHERE institue LIKE '$kw';";
+        $sql_all = "SELECT * FROM res_employment WHERE institute LIKE '$kw' ORDER BY institute;";
     } elseif ($field_search == 'expertise') {
-        $sql_all = "SELECT * FROM res_expertise
-            WHERE topic LIKE '$kw';";
+        $sql_all = "SELECT * FROM res_expertise WHERE (topic LIKE '$kw') OR (specific_topic LIKE '$kw');";
     }
-
     $result_all = mysql_query($sql_all);
-    
 } // END if
 ?>
 
@@ -37,19 +30,21 @@ if (isset($_POST['keyword']) && !empty($_POST['keyword'])) {
         <div class="row" style="margin-top: 10px;">
             <h2 class="text-center">ฐานข้อมูลนักวิจัย</h2>
 
+            <p>&nbsp;</p>
+
             <!-- =============================== Search Researcher ================================= -->
             <div class="col-md-12  well" style="border: solid 1px #ddd;">
 
-                <h3 class="text-center"><span class="glyphicon glyphicon-search"></span> Search</h3>
+                <h3 class="text-center"><span class="glyphicon glyphicon-search"></span> Search ค้นหานักวิจัย</h3>
                 <form role="form" name="form1" method="post" action="">
 
                     <div class="row">
                         <div class="col-md-1" style=""><strong>ค้นหาจาก</strong></div>
                         <div class="col-md-2" style="">
                             <select name="field_search" class="form-control">
-                                <option value="firstname">ชื่อ</option>
+                                <option value="firstname" selected>ชื่อ</option>
                                 <option value="lastname">นามสกุล</option>
-                                <option value="institue">สถาบัน</option>
+                                <option value="institute">สถาบัน</option>
                                 <option value="expertise">ความเชี่ยวชาญ</option>
                             </select>
                         </div>
@@ -66,21 +61,182 @@ if (isset($_POST['keyword']) && !empty($_POST['keyword'])) {
 
 
             <?php if (isset($result_all)): ?>
-                <div class="col-md-12 alert alert-success" role="alert"" style="border: solid 1px #ddd; margin-top: 20px;">
+                <div class="col-md-12" style="margin-top: 20px;">
                     <h3>ผลการค้นหา</h3>
+                    คำค้นหา <strong style="color: blue;"><?php echo $kw_ori; ?></strong>
+
                     <?php if (mysql_num_rows($result_all) > 0): ?>
-                        คำค้นหา <strong style="color: blue;"><?php echo $kw_ori; ?></strong>
-                        <br>ค้นหาจาก <strong>ชื่อ (firstname)</strong>
-                        <br>พบนักวิจัยทั้งหมด <span style="color: green;"><?php echo mysql_num_rows($result_all); ?></span> ท่าน ดังนี้
-                        <?php $i = 1; ?>
-                        <?php while ($r_all = mysql_fetch_object($result_all)) : ?>
-                            <p><?php echo $i . '. ' . $r_all->firstname_en . ' ' . $r_all->lastname_en . ' ' . $r_all->firstname_th . ' ' . $r_all->lastname_th; ?> </p>
-                            <?php $i++; ?>
-                        <?php endwhile; ?>
+
+                        <?php if ($field_search == 'firstname') : ?>
+                            <br>ค้นหาจาก <strong>ชื่อ (firstname)</strong> 
+                            พบนักวิจัยทั้งหมด <span style="color: green;"><?php echo mysql_num_rows($result_all); ?></span> ท่าน ดังนี้
+                            <?php $i = 1; ?>
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th width="12">No.</th>
+                                        <th>ชื่อ นามสกุล (firstname lastname)</th>
+                                        <th>สถาบัน (institute)</th>
+                                    </tr>
+                                    <?php while ($r_all = mysql_fetch_object($result_all)) : ?>
+                                        <?php $sql_em = "SELECT * FROM res_employment WHERE researcher_id = $r_all->researcher_id ORDER BY institute;"; ?>
+                                        <?php $result_em = mysql_query($sql_em); ?>
+
+                                        <?php if (mysql_num_rows($result_em) > 0): ?>
+                                            <?php while ($r_em = mysql_fetch_object($result_em)): ?>
+
+                                                <tr>
+                                                    <td><?php echo $i; ?></td>
+                                                    <td>
+                                                        <a href="researcher_profile.php?id=<?php echo $r_all->researcher_id; ?>">
+                                                            <?php echo $r_all->title_en . $r_all->firstname_en . ' ' . $r_all->lastname_en . '<br>' . $r_all->title_th . $r_all->firstname_th . ' ' . $r_all->lastname_th; ?>
+                                                        </a>
+                                                    </td>
+                                                    <td><?php echo $r_em->institute; ?></td>
+                                                </tr>
+
+                                            <?php endwhile; ?>
+                                        <?php else: ?>
+                                            <tr>
+                                                <td><?php echo $i; ?></td>
+                                                <td>
+                                                    <a href="researcher_profile.php?id=<?php echo $r_all->researcher_id; ?>">
+                                                        <?php echo $r_all->title_en . $r_all->firstname_en . ' ' . $r_all->lastname_en . '<br>' . $r_all->title_th . $r_all->firstname_th . ' ' . $r_all->lastname_th; ?>
+                                                    </a>
+                                                </td>
+                                                <td> - </td>
+                                            </tr>
+                                        <?php endif; ?>
+
+                                        <?php $i++; ?>
+                                    <?php endwhile; ?>
+                                </tbody>
+                            </table>
+
+                        <?php elseif ($field_search == 'lastname') : ?>
+                            <br>ค้นหาจาก <strong>นามสกุล (laststname)</strong> 
+                            พบนักวิจัยทั้งหมด <span style="color: green;"><?php echo mysql_num_rows($result_all); ?></span> ท่าน ดังนี้
+                            <?php $i = 1; ?>
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th width="12">No.</th>
+                                        <th>ชื่อ นามสกุล (firstname lastname)</th>
+                                        <th>สถาบัน (institute)</th>
+                                    </tr>
+                                    <?php while ($r_all = mysql_fetch_object($result_all)) : ?>
+                                        <?php $sql_em = "SELECT * FROM res_employment WHERE researcher_id = $r_all->researcher_id ORDER BY institute;"; ?>
+                                        <?php $result_em = mysql_query($sql_em); ?>
+
+                                        <?php if (mysql_num_rows($result_em) > 0): ?>
+                                            <?php while ($r_em = mysql_fetch_object($result_em)): ?>
+
+                                                <tr>
+                                                    <td><?php echo $i; ?></td>
+                                                    <td>
+                                                        <a href="researcher_profile.php?id=<?php echo $r_all->researcher_id; ?>">
+                                                            <?php echo $r_all->title_en . $r_all->firstname_en . ' ' . $r_all->lastname_en . '<br>' . $r_all->title_th . $r_all->firstname_th . ' ' . $r_all->lastname_th; ?>
+                                                        </a>
+                                                    </td>
+                                                    <td><?php echo $r_em->institute; ?></td>
+                                                </tr>
+
+                                            <?php endwhile; ?>
+                                        <?php else: ?>
+                                            <tr>
+                                                <td><?php echo $i; ?></td>
+                                                <td>
+                                                    <a href="researcher_profile.php?id=<?php echo $r_all->researcher_id; ?>">
+                                                        <?php echo $r_all->title_en . $r_all->firstname_en . ' ' . $r_all->lastname_en . '<br>' . $r_all->title_th . $r_all->firstname_th . ' ' . $r_all->lastname_th; ?>
+                                                    </a>
+                                                </td>
+                                                <td> - </td>
+                                            </tr>
+                                        <?php endif; ?>
+
+                                        <?php $i++; ?>
+                                    <?php endwhile; ?>
+                                </tbody>
+                            </table>
+
+                        <?php elseif ($field_search == 'institute') : ?>
+                            <br>ค้นหาจาก <strong>สถาบัน (institute)</strong> 
+                            พบนักวิจัยทั้งหมด <span style="color: green;"><?php echo mysql_num_rows($result_all); ?></span> ท่าน ดังนี้
+
+                            <?php $i = 1; ?>
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th width="12">No.</th>
+                                        <th>ชื่อ นามสกุล (firstname lastname)</th>
+                                        <th>สถาบัน (institute)</th>
+                                    </tr>
+                                    <?php while ($r_all = mysql_fetch_object($result_all)) : ?>
+                                        <?php $sql_pro = "SELECT * FROM res_profile WHERE researcher_id = $r_all->researcher_id ORDER BY firstname_en;"; ?>
+                                        <?php $result_pro = mysql_query($sql_pro); ?>
+
+                                        <?php while ($r_pro = mysql_fetch_object($result_pro)): ?>
+
+                                            <tr>
+                                                <td><?php echo $i; ?></td>
+                                                <td>
+                                                    <a href="researcher_profile.php?id=<?php echo $r_pro->researcher_id; ?>">
+                                                        <?php echo $r_pro->title_en . $r_pro->firstname_en . ' ' . $r_pro->lastname_en . '<br>' . $r_pro->title_th . $r_pro->firstname_th . ' ' . $r_pro->lastname_th; ?>
+                                                    </a>
+                                                </td>
+                                                <td><?php echo $r_all->institute; ?></td>
+                                            </tr>
+
+                                        <?php endwhile; ?>
+
+                                        <?php $i++; ?>
+                                    <?php endwhile; ?>
+                                </tbody>
+                            </table>
+
+
+                        <?php elseif ($field_search == 'expertise') : ?>
+                            <br>ค้นหาจาก <strong>ความเชี่ยวชาญ (expertise)</strong>
+                            <br>พบนักวิจัยทั้งหมด <span style="color: green;"><?php echo mysql_num_rows($result_all); ?></span> ท่าน ดังนี้
+                            <?php $i = 1; ?>
+
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th width="12">No.</th>
+                                        <th>ชื่อ นามสกุล (firstname lastname)</th>
+                                        <th>ความเชี่ยวชาญ (expertise)</th>
+                                        <th>ความเชี่ยวชาญเฉพาะ (specific)</th>
+                                    </tr>
+                                    <?php while ($r_all = mysql_fetch_object($result_all)) : ?>
+
+                                        <?php $sql_pro = "SELECT * FROM res_profile WHERE researcher_id = $r_all->researcher_id ORDER BY firstname_en DESC;"; ?>
+                                        <?php $result_pro = mysql_query($sql_pro); ?>                                
+                                        <?php while ($r_pro = mysql_fetch_object($result_pro)): ?>
+
+                                            <tr>
+                                                <td><?php echo $i; ?></td>
+                                                <td>
+                                                    <a href="researcher_profile.php?id=<?php echo $r_pro->researcher_id; ?>">
+                                                        <?php echo $r_pro->title_en . $r_pro->firstname_en . ' ' . $r_pro->lastname_en . '<br>' . $r_pro->title_th . $r_pro->firstname_th . ' ' . $r_pro->lastname_th; ?>
+                                                    </a>
+                                                </td>
+                                                <td><?php echo $r_all->topic; ?></td>
+                                                <td><?php echo $r_all->specific_topic; ?></td>
+                                            </tr>
+
+                                        <?php endwhile; ?>
+
+                                        <?php $i++; ?>
+                                    <?php endwhile; ?>
+                                </tbody>
+                            </table>
+
+                        <?php endif; ?>
 
                     <?php else : ?>
 
-                        <strong style="color: red;">ขออภัย ไม่พบข้อมูล</strong>
+                        <p><strong style="color: red;">ขออภัย ไม่พบข้อมูล</strong></p>
 
                     <?php endif; ?>
                     <p>&nbsp;</p>
@@ -90,9 +246,7 @@ if (isset($_POST['keyword']) && !empty($_POST['keyword'])) {
 
 
 
-        </div> <!-- /.row -->
-
-
+        </div> <!-- /.row -->       
 
 
         <?php get_includes('footer'); ?>
---
+++ src/views/researcher_profile.php
@@ -0,0 +1,196 @@
+<?php
+require_once '../system/system.php';
+
+if (isset($_GET['id'])) {
+    $id = htmlspecialchars($_GET['id']);
+    if ($id < 1) {
+        header("location: researcher.php");
+    }
+} else {
+    header("location: researcher.php");
+}
+
+$sql_pro = "SELECT * FROM res_profile WHERE researcher_id = $id;";
+$result_pro = mysql_query($sql_pro);
+if (mysql_num_rows($result_pro) < 1) {
+    header("location: researcher.php");
+}
+$r_pro = mysql_fetch_object($result_pro);
+
+$sql_em = "SELECT * FROM res_employment WHERE researcher_id = $id;";
+$result_em = mysql_query($sql_em);
+$r_em = mysql_fetch_object($result_em);
+
+$sql_ed = "SELECT * FROM res_education WHERE researcher_id = $id;";
+$result_ed = mysql_query($sql_ed);
+
+$sql_ex = "SELECT * FROM res_expertise WHERE researcher_id = $id;";
+$result_ex = mysql_query($sql_ex);
+
+$sql_pub = "SELECT * FROM res_publication WHERE researcher_id = $id;";
+$result_pub = mysql_query($sql_pub);
+
+doc_head('ฐานข้อมูลนักวิจัย ศูนย์ความเป็นเลิศด้านฟิสิกส์');
+?>
+
+</head>
+
+<body>
+    <div class="container">
+
+        <?php get_includes('header'); ?>
+
+        <div class="row">
+
+            <div class="col-md-12">
+
+                <h2 class="text-center">ฐานข้อมูลนักวิจัย</h2>
+                <p>&nbsp;</p>
+
+                <div class="row">
+
+                    <div class="col-md-2">
+                        <img style="max-width: 100%;" src="<?php echo $r_pro->pic_url; ?>">
+                    </div>
+
+                    <div class="col-md-10">
+                        <p><strong>ชื่อ:</strong> <?php echo $r_pro->title_th . $r_pro->firstname_th . ' ' . $r_pro->lastname_th; ?></p>
+                        <p><strong>Name:</strong> <?php echo $r_pro->title_en . $r_pro->firstname_en . ' ' . $r_pro->lastname_en; ?></p>
+                        <p>&nbsp;</p>
+
+                        <h5>ที่ติดต่อ</h5>
+                        <p><strong>ที่ทำงาน (Thai):</strong>
+                            <?php if (!empty($r_em->street_th)): ?>
+                                <br>
+                                <?php echo $r_em->street_th; ?>
+
+                                <?php if (!empty($r_em->sub_district_th)): ?>
+                                    แขวง/ตำบล <?php echo $r_em->sub_district_th; ?>
+                                <?php endif; ?>
+
+                                เขต/อำเภอ <?php echo $r_em->district_th; ?>
+                                จังหวัด <?php echo $r_em->province_th; ?>
+                                <?php echo $r_em->postal_code; ?>
+
+                            <?php else: ?>
+                                -
+                            <?php endif; ?>
+                        </p>
+                        <p>&nbsp;</p>
+
+                        <p><strong>ที่ทำงาน (English):</strong>
+                            <?php if (!empty($r_em->street_en)): ?> 
+                                <br>
+                                <?php echo $r_em->street_en; ?>                        
+                                <br><?php echo $r_em->sub_district_en; ?>,
+                                <?php echo $r_em->district_en; ?>,
+                                <?php echo $r_em->province_en; ?>,
+                                <?php echo $r_em->postal_code; ?>
+
+                            <?php else: ?>
+                                -
+                            <?php endif; ?>
+                        </p>
+                        <p>&nbsp;</p>
+                        
+                        <p>
+                            <strong>โทรศัพท์:</strong> 
+                            <?php
+                            if (!empty($r_em->phone)) {
+                                echo $r_em->phone;
+                            }
+                            ?>
+                        </p>
+
+                        <p>
+                            <strong>Email:</strong> 
+                            <?php
+                            if (!empty($r_em->email)) {
+                                echo $r_em->email;
+                            }
+                            ?>
+                        </p>
+                        <p>
+                            <strong>Website:</strong>
+                            <?php if (!empty($r_em->website)) : ?>
+                                <a target="_blank" href="<?php echo $r_em->website; ?>"><?php echo $r_em->website; ?></a>
+                            <?php endif; ?>
+                        </p>
+                        <p>&nbsp;</p>
+
+                        <h5>การศึกษา</h5>
+                        <table class="table table-bordered">
+                            <tbody>
+                                <tr>
+                                    <th width="160">ระดับการศึกษา (degree)</th><th>สาขา</th><th width="60">ปีที่จบ</th><th>สถาบันการศึกษา</th><th>ประเทศ</th>
+                                </tr>
+                                <?php if (mysql_num_rows($result_ed) > 0) : ?>
+
+                                    <?php while ($r_ed = mysql_fetch_object($result_ed)): ?>
+                                        <tr>
+                                            <td><?php echo ucfirst($r_ed->degree); ?>'s degree</td>
+                                            <td><?php echo $r_ed->major; ?></td>
+                                            <td><?php echo $r_ed->grad_year; ?></td>
+                                            <td><?php echo $r_ed->institute; ?></td>
+                                            <td><?php echo $r_ed->country; ?></td>
+                                        </tr>
+                                    <?php endwhile; ?>
+
+                                <?php else: ?>
+                                    <tr>
+                                        <td colspan="5" style="text-align: center;">- ไม่พบข้อมูลการศึกษา -</td>
+                                    </tr>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                        <p>&nbsp;</p>
+
+                        <h5>ความเชี่ยวชาญ (Field of Expertise/Competency)</h5>
+                        <?php if (mysql_num_rows($result_ex) > 0): ?>
+                            <?php $r_ex = mysql_fetch_object($result_ex); ?>
+                            <p>
+                                <?php if (!empty($r_ex->topic)): ?>
+                                    <?php echo $r_ex->topic; ?>
+                                    <br>
+                                <?php endif; ?>
+                                - <?php echo $r_ex->specific_topic; ?>
+                            </p>
+                        <?php else: ?>
+                            <p>- ขออภัย ไม่พบข้อมูล -</p>
+                        <?php endif; ?>
+                        <p>&nbsp;</p>
+
+                        <h5>Publication</h5> 
+                        (ผลงานตีพิมพ์ย้อนหลัง 10 ปี)
+                        <?php if (mysql_num_rows($result_pub) > 0): ?>
+                            <?php $r_pub = mysql_fetch_object($result_pub); ?>
+                            <p><?php echo htmlspecialchars_decode($r_pub->content); ?></p>
+                        <?php else: ?>
+                            <p>- ขออภัย ไม่พบข้อมูล -</p>
+                        <?php endif; ?>
+                        <p>&nbsp;</p>
+
+
+                    </div>
+                </div> <!-- /.row -->
+
+
+
+
+            </div>
+
+        </div> <!-- /.row -->
+
+        <div class="row">
+            <div class="col-md-12">
+                <a href="researcher.php" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span> กลับไปหน้าค้นหานักวิจัย</a>
+            </div>
+        </div>
+
+        <?php get_includes('footer'); ?>
+
+    </div> <!-- /.container -->
+
+    <?php get_includes('bootstrap-core'); ?>
+</body>
+</html>
