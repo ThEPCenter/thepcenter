3cac90378b73eb35d650760c4dfe0978ede704ad
fisca <fisca2012@gmail.com>
2015-03-27 17:14:00 +0700

    Update file name in popup.

--- src/views/home.php
+++ src/views/home.php
@@ -109,7 +109,7 @@ if ($_SESSION['fancy'] == "first") {
             <div class="col-xs-6 col-sm-6 col-md-6" style="padding-right: 0; padding-left: 0;">
                 <a title="การจัดสัมมนา เรื่อง วัสดุเทอร์โมอิเล็กทริกโครงสร้างนาโนฟิล์มบาง วันจันทร์ที่ 30 มีนาคม 2558" 
                    href="http://thep-center.org/src2/views/notice.php?news_id=72">
-                    <img alt="Thermoelectric" style="max-width: 100%" src="http://thep-center.org/upload02/Thermoelectric_150311.jpg">
+                    <img alt="Thermoelectric" style="max-width: 100%" src="http://thep-center.org/upload02/Thermoelectric_150311_02.jpg">
                 </a><br>
                 <a title="การจัดสัมมนา Consultant Meeting เรื่อง โปรแกรมวิจัย Magnetic Materials วันอังคารที่ 31 มีนาคม 2558" 
                    href="http://thep-center.org/src2/views/notice.php?news_id=70">
